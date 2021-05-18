892d012bf1ec22d8232639fde7abd684746bb61c
athulreji <athulreji007@gmail.com>
2021-05-18 17:35:27 +0530

    signup connected to db

--- twitter_assist/mainapp/views.py
+++ twitter_assist/mainapp/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from .models import User
 
 def index(request):
     return render(request, 'index.html')
 
 def signup(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        name = str(request.POST.get('firstname')) + ' ' + str(request.POST.get('lastname'))
+        twitterid = request.POST.get('twitterid')
+        password = request.POST.get('password')
+        user = User(email=email, name=name, twitterid=twitterid, password=password)
+        user.save()
+        return redirect('index')
     return render(request, 'signup.html')
