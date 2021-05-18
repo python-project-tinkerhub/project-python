0244e5c543ffe992da31ab7c0fcc26b34ef9145f
athulreji <athulreji007@gmail.com>
2021-05-18 12:34:34 +0530

    database added

--- twitter_assist/mainapp/views.py
+++ twitter_assist/mainapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("<h1>this is index page</h1>")
+
+def signup(request):
+    return HttpResponse("This is Signup page.")
