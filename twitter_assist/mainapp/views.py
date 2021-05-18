6703ef0b21d3e6d10f19f7ada6543b1d4cf33d74
athulreji <athulreji007@gmail.com>
2021-05-18 20:59:00 +0530

    login connected to db

--- twitter_assist/mainapp/views.py
+++ twitter_assist/mainapp/views.py
@@ -3,6 +3,15 @@ from django.http import HttpResponse
 from .models import User
 
 def index(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        if User.objects.filter(email=email).count()!=0:
+            if User.objects.filter(email=email, password=password).count() ==1:
+                request.session['email'] = email
+                return redirect('dashboard')
+            return render(request, 'index.html', {'error': 'wpwd'})
+        return render(request, 'index.html', {'error': 'wemail'})
     return render(request, 'index.html')
 
 def signup(request):
@@ -15,3 +24,13 @@ def signup(request):
         user.save()
         return redirect('index')
     return render(request, 'signup.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def logout(request):
+    try:
+        del request.session['email']
+    except:
+        pass
+    return redirect('index')
